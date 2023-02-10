545d6e34a32c70a0f2c998f508e32e786b107227
Guillaume 'Yugo' Coguiec <guillaume@logical.work>
2023-02-09 20:58:11 -0500

    release: 1.1.0.

--- index.cjs
+++ index.cjs
@@ -1,23 +1,28 @@
 module.exports = {
-  plugins: ['stylelint-order', 'stylelint-scss', 'stylelint-prettier'],
+  plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
     // stylelint-core
+    'alpha-value-notation': [
+      'percentage',
+      {
+        exceptProperties: [
+          'opacity',
+          'fill-opacity',
+          'flood-opacity',
+          'stop-opacity',
+          'stroke-opacity'
+        ]
+      }
+    ],
     'at-rule-empty-line-before': null,
-    'at-rule-name-case': 'lower',
-    'at-rule-name-space-after': 'always-single-line',
-    'at-rule-semicolon-newline-after': 'always',
     'at-rule-no-unknown': null,
-    'block-closing-brace-empty-line-before': 'never',
-    'block-closing-brace-newline-after': null,
-    'block-closing-brace-newline-before': 'always-multi-line',
-    'block-closing-brace-space-before': 'always-single-line',
-    'block-opening-brace-newline-after': 'always-multi-line',
-    'block-opening-brace-space-after': 'always-single-line',
-    'block-opening-brace-space-before': 'always',
-    'block-no-empty': true,
+    'at-rule-no-vendor-prefix': true,
     'color-hex-case': 'lower',
+    'block-no-empty': true,
     'color-hex-length': 'short',
     'color-no-invalid-hex': true,
+    'color-function-notation': 'modern',
+    'custom-property-no-missing-var-function': true,
     'comment-empty-line-before': [
       'always',
       {
@@ -33,6 +38,20 @@ module.exports = {
         ignore: ['after-comment', 'inside-single-line-block']
       }
     ],
+    'custom-media-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: name =>
+          `Expected custom media query name "${name}" to be kebab-case`
+      }
+    ],
+    'custom-property-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: name =>
+          `Expected custom property name "${name}" to be kebab-case`
+      }
+    ],
     'comment-no-empty': true,
     'declaration-block-no-duplicate-properties': [
       true,
@@ -40,17 +59,10 @@ module.exports = {
         ignore: ['consecutive-duplicates-with-different-values']
       }
     ],
+    'declaration-block-no-duplicate-custom-properties': true,
+    'declaration-block-no-redundant-longhand-properties': true,
     'declaration-block-no-shorthand-property-overrides': true,
-    'declaration-bang-space-after': 'never',
-    'declaration-bang-space-before': 'always',
-    'declaration-block-semicolon-newline-after': 'always-multi-line',
-    'declaration-block-semicolon-space-after': 'always-single-line',
-    'declaration-block-semicolon-space-before': 'never',
     'declaration-block-single-line-max-declarations': 1,
-    'declaration-block-trailing-semicolon': 'always',
-    'declaration-colon-newline-after': null,
-    'declaration-colon-space-after': 'always-single-line',
-    'declaration-colon-space-before': 'never',
     'declaration-empty-line-before': [
       'always',
       {
@@ -60,42 +72,39 @@ module.exports = {
     ],
     'font-family-no-duplicate-names': true,
     'font-family-no-missing-generic-family-keyword': true,
+    'font-family-name-quotes': 'always-where-recommended',
     'function-calc-no-unspaced-operator': true,
     'function-linear-gradient-no-nonstandard-direction': true,
-    'function-comma-newline-after': 'always-multi-line',
-    'function-comma-space-after': 'always-single-line',
-    'function-comma-space-before': 'never',
-    'function-max-empty-lines': 0,
+    'function-no-unknown': true,
     'function-name-case': 'lower',
-    'function-parentheses-newline-inside': 'always-multi-line',
-    'function-parentheses-space-inside': 'never-single-line',
-    'function-whitespace-after': 'always',
-    indentation: 2,
-    'length-zero-no-unit': true,
+    'function-url-quotes': 'always',
+    'hue-degree-notation': 'angle',
+    'keyframe-selector-notation': 'percentage-unless-within-keyword-only-block',
+    'keyframe-block-no-duplicate-selectors': true,
     'keyframe-declaration-no-important': true,
-    'max-empty-lines': 1,
-    'media-feature-colon-space-after': 'always',
-    'media-feature-colon-space-before': 'never',
-    'media-feature-name-case': 'lower',
-    'media-feature-parentheses-space-inside': 'never',
-    'media-feature-range-operator-space-after': 'always',
-    'media-feature-range-operator-space-before': 'always',
-    'media-query-list-comma-newline-after': 'always-multi-line',
-    'media-query-list-comma-space-after': 'always-single-line',
-    'media-query-list-comma-space-before': 'never',
+    'keyframes-name-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: name => `Expected keyframe name "${name}" to be kebab-case`
+      }
+    ],
+    'length-zero-no-unit': [
+      true,
+      {
+        ignore: ['custom-properties']
+      }
+    ],
+    'media-feature-name-no-vendor-prefix': true,
     'media-feature-name-no-unknown': true,
+    'named-grid-areas-no-invalid': true,
     'no-descending-specificity': null,
     'no-duplicate-at-import-rules': true,
     'no-duplicate-selectors': true,
     'no-empty-source': null,
-    'no-extra-semicolons': true,
     'no-invalid-double-slash-comments': true,
-    'no-eol-whitespace': true,
-    'no-missing-end-of-source-newline': true,
-    'number-leading-zero': 'always',
+    'no-invalid-position-at-import-rule': true,
+    'no-irregular-whitespace': true,
     'number-max-precision': 5,
-    'number-no-trailing-zeros': true,
-    'property-case': 'lower',
     'property-no-unknown': true,
     'rule-empty-line-before': [
       'always-multi-line',
@@ -104,33 +113,26 @@ module.exports = {
         ignore: ['after-comment']
       }
     ],
+    'selector-attribute-quotes': 'always',
     'selector-pseudo-class-no-unknown': true,
     'selector-pseudo-element-no-unknown': null,
     'selector-type-no-unknown': null,
-    'selector-attribute-brackets-space-inside': 'never',
-    'selector-attribute-operator-space-after': 'never',
-    'selector-attribute-operator-space-before': 'never',
-    'selector-combinator-space-after': 'always',
-    'selector-combinator-space-before': 'always',
-    'selector-descendant-combinator-no-non-space': true,
-    'selector-list-comma-newline-after': 'always',
-    'selector-list-comma-space-before': 'never',
-    'selector-max-empty-lines': 0,
     'selector-max-compound-selectors': 3,
     'selector-max-specificity': '0,4,0',
-    'selector-pseudo-class-case': 'lower',
-    'selector-pseudo-class-parentheses-space-inside': 'never',
-    'selector-pseudo-element-case': 'lower',
     'selector-pseudo-element-colon-notation': 'double',
     'selector-type-case': 'lower',
+    'selector-no-vendor-prefix': true,
+    'selector-not-notation': 'complex',
+    'shorthand-property-no-redundant-values': true,
     'string-no-newline': true,
     'unit-no-unknown': true,
-    'unit-case': 'lower',
     'value-keyword-case': 'lower',
-    'value-list-comma-newline-after': null,
-    'value-list-comma-space-after': 'always-single-line',
-    'value-list-comma-space-before': 'never',
-    'value-list-max-empty-lines': 0,
+    'value-no-vendor-prefix': [
+      true,
+      {
+        ignoreValues: ['box', 'inline-box']
+      }
+    ],
 
     // stylelint-scss
     'scss/at-each-key-value-single-line': true,
@@ -148,10 +150,6 @@ module.exports = {
     'scss/double-slash-comment-whitespace-inside': 'always',
 
     // stylelint-order
-    'order/order': ['custom-properties', 'declarations'],
-    'order/properties-alphabetical-order': true,
-
-    // stylelint-prettier
-    'prettier/prettier': true
+    'order/order': ['custom-properties', 'declarations']
   }
 };
--- index.spec.mjs
+++ index.spec.mjs
@@ -36,48 +36,6 @@ it('Allows no empty line before an at-rule', async () => {
   expect(results).not.toHaveAnError('at-rule-empty-line-before');
 });
 
-it('Does not accept non-lowercase at-rules', async () => {
-  const { results, fixed } = await lint`
-    @Media (min-width: 50em) {}
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('at-rule-name-case', {
-    line: 2,
-    column: 5,
-    text: 'Expected "Media" to be "media" (at-rule-name-case)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
-it('Requires a single space after at-rule names in single-line blocks', async () => {
-  const { results, fixed } = await lint`
-    @media(min-width: 50em) {}
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('at-rule-name-space-after', {
-    line: 2,
-    column: 10,
-    text: 'Expected single space after at-rule name "@media" (at-rule-name-space-after)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
-it('Requires a newline after the semicolon of an at-rule', async () => {
-  const { results, fixed } = await lint`
-    @import url("x.css"); a {}
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('at-rule-semicolon-newline-after', {
-    line: 2,
-    column: 26,
-    text: 'Expected newline after ";" (at-rule-semicolon-newline-after)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
 it('Accepts scss at-rules', async () => {
   const { results } = await lint`
     $flag: true;
@@ -91,23 +49,6 @@ it('Accepts scss at-rules', async () => {
   expect(results).not.toHaveAnError('at-rule-no-unknown');
 });
 
-it('Disallows an empty line before closing blocks', async () => {
-  const { results, fixed } = await lint`
-    a {
-      color: black;
-
-    }
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('block-closing-brace-empty-line-before', {
-    line: 5,
-    column: 5,
-    text: 'Unexpected empty line before closing brace (block-closing-brace-empty-line-before)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
 it('Allows a newline after a closing brace', async () => {
   const { results } = await lint`
     a { color: pink; }
@@ -117,78 +58,6 @@ it('Allows a newline after a closing brace', async () => {
 
   expect(results).toHaveBeenParsed();
   expect(results).not.toHaveAnError('block-closing-brace-newline-after');
-});
-
-it('Requires a newline before a closing brace only in a multi-line context', async () => {
-  const { results, fixed } = await lint`
-    a {
-      color: pink;}
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('block-closing-brace-newline-before', {
-    line: 3,
-    column: 18,
-    text: 'Expected newline before "}" of a multi-line block (block-closing-brace-newline-before)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
-it('Requires a space before a closing brace only in a single-line context', async () => {
-  const { results, fixed } = await lint`
-    a { color: pink;}
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('block-closing-brace-space-before', {
-    line: 2,
-    column: 20,
-    text: 'Expected single space before "}" of a single-line block (block-closing-brace-space-before)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
-it('Requires a newline after an opening brace only in a single-line context', async () => {
-  const { results, fixed } = await lint`
-    a{color: pink;
-    }
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('block-opening-brace-newline-after', {
-    line: 2,
-    column: 7,
-    text: 'Expected newline after "{" of a multi-line block (block-opening-brace-newline-after)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
-it('Requires a space after a closing brace only in a single-line context', async () => {
-  const { results, fixed } = await lint`
-    a {color: pink; }
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('block-opening-brace-space-after', {
-    line: 2,
-    column: 8,
-    text: 'Expected single space after "{" of a single-line block (block-opening-brace-space-after)'
-  });
-  expect(fixed).toMatchSnapshot();
-});
-
-it('Requires a space before a closing brace only in a single-line context', async () => {
-  const { results, fixed } = await lint`
-    a{ color: pink; }
-  `;
-
-  expect(results).toHaveBeenParsed();
-  expect(results).toHaveAnError('block-opening-brace-space-before', {
-    line: 2,
-    column: 5,
-    text: 'Expected single space before "{" (block-opening-brace-space-before)'
-  });
-  expect(fixed).toMatchSnapshot();
 });
 
 it('Disallows empty blocks', async () => {
